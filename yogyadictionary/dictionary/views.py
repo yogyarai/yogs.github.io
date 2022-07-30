f514f5b2242250f9fd1256a1b5a3b985537fb3e7
Yogya <yogyarai6270@gmail.com>
2022-07-30 09:35:24 +0530

    commit

---
+++ yogyadictionary/dictionary/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from PyDictionary import PyDictionary
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def word(request):
+    search = request.GET.get('search')
+    dictionary = PyDictionary()
+    meaning = dictionary.meaning(search)
+
+    context = {
+        'meaning': meaning['Noun'][0]
+
+    }
+    return render(request, 'word.html', context)
